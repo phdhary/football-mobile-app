6e44fe801e56699010fd1229222faf5ed5d5f70a
Hary <phdhary@gmail.com>
2020-11-13 06:35:51 +0700

    add color variable to be used later

---
+++ lib/shared/theme.dart
@@ -0,0 +1,10 @@
+part of 'shared.dart';
+
+Color whiteBone = Color(0xffF8F8F8);
+Color red = Color(0xffFF4D4D);
+Color purple = Color(0xffFF4D4D);
+Color yellow = Color(0xffFFB74C);
+Color winGradientColor1 = Color(0xffBFD1FF);
+Color winGradientColor2 = Color(0xff9180FF);
+Color black = Color(0xff262626);
+Color gray = Color(0xffBFBFBF);
